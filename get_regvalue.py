f17e8aaa391e89b8f90fdb60c8b45d77432742bf
dmzkrsk <dmzkrsk@gmail.com>
2014-12-29 15:21:57 +0700

    Update get_regvalue.py
    32bit python doesn't see 64bit registry by default

--- get_regvalue.py
+++ get_regvalue.py
@@ -4,7 +4,9 @@ def get_regvalue(regkey, regvalue):
 
     explorer = _winreg.OpenKey(
         _winreg.HKEY_LOCAL_MACHINE,
-        regkey
+        regkey,
+        0,
+        _winreg.KEY_READ | _winreg.KEY_WOW64_64KEY
     )
     
     value, type = _winreg.QueryValueEx(explorer, regvalue)
