d1270b555cfaf5b0c9108e3c46fcdc1f6c3546ea
dabreve <brent.verner@gmail.com>
2015-04-03 20:54:34 +0000

    add support for background color in convert

--- src/imagemagick.cc
+++ src/imagemagick.cc
@@ -9,6 +9,7 @@
 #include "imagemagick.h"
 #include <list>
 #include <sstream>
+#include <iostream>
 #include <string.h>
 #include <exception>
 
@@ -87,6 +88,7 @@ struct convert_im_ctx : im_ctx_base {
     std::string format;
     std::string filter;
     std::string blur;
+    std::string background;
     unsigned int quality;
     int rotate;
     int density;
@@ -171,6 +173,19 @@ void DoConvert(uv_work_t* req) {
     Magick::Blob srcBlob( context->srcData, context->length );
 
     Magick::Image image;
+
+    if( ! context->background.empty() ){
+        try {
+            Magick::Color bg( context->background.c_str() );
+            image.backgroundColor(bg);
+            if( debug ){
+                std::cerr << "Background: " << static_cast<std::string>(bg) << std::endl;
+            }
+        }
+        catch( Magick::WarningOption &warning ){
+            std::cerr << "Warning: " << warning.what() << std::endl;
+        }
+    }
 
     if ( !ReadImageMagick(&image, srcBlob, context->srcFormat, context) )
         return;
@@ -465,6 +480,7 @@ void GeneratedBlobAfter(uv_work_t* req) {
 //                                         "Center", "East", "SouthWest", "South", "SouthEast", "None"
 //                  format:      optional. one of http://www.imagemagick.org/script/formats.php ex: "JPEG"
 //                  filter:      optional. ex: "Lagrange", "Lanczos". see ImageMagick's magick/option.c for candidates
+//                  background:  optional. ex: "none", "transparent", "red", "green", "#F00", "#00ff00"
 //                  blur:        optional. ex: 0.8
 //                  strip:       optional. default: false. strips comments out from image.
 //                  maxMemory:   optional. set the maximum width * height of an image that can reside in the pixel cache memory.
@@ -545,6 +561,10 @@ NAN_METHOD(Convert) {
     Local<Value> filterValue = obj->Get( NanNew<String>("filter") );
     context->filter = !filterValue->IsUndefined() ?
         *NanAsciiString(filterValue) : "";
+
+    Local<Value> backgroundValue = obj->Get( NanNew<String>("background") );
+    context->background = !backgroundValue->IsUndefined() ?
+        *NanAsciiString(backgroundValue) : "";
 
     uv_work_t* req = new uv_work_t();
     req->data = context;
