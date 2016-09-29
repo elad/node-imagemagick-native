1d98e0c8bd547de1e390ee69a5894af7c8fb87a2
Johnathan Leppert <johnathan@kespry.com>
2016-09-29 14:15:47 -0700

    streamify identify method

--- index.js
+++ index.js
@@ -5,35 +5,45 @@ module.exports = require(__dirname + '/build/Release/imagemagick.node');
 var stream = require('stream');
 var util = require('util');
 
-function Convert(options) {
-  // allow use without new
-  if (!(this instanceof Convert)) {
-    return new Convert(options);
+function getStreamHandler(method, readableObjectMode) {
+  function MagickStream(options) {
+    // allow use without new
+    if (!(this instanceof MagickStream)) {
+      return new MagickStream(options);
+    }
+    
+    options = options || {};
+    if(readableObjectMode) options.readableObjectMode = true;
+    this._options = options || {};
+    this._bufs = [];
+    this._method = method;
+
+    // init Transform
+    stream.Transform.call(this, options);
+  }
+  util.inherits(MagickStream, stream.Transform);
+  
+  MagickStream.prototype._transform = function (chunk, enc, done) {
+    this._bufs.push(chunk);
+    done();
+  };
+
+  MagickStream.prototype._flush = function(done) {
+    this._options.srcData = Buffer.concat(this._bufs);
+    var self = this;
+    this._method(this._options,function(err,data){
+      if(err) {
+        return done(err);
+      }
+      self.push(data);
+      done();
+    });
   }
 
-  this._options = options;
-  this._bufs = [];
-
-  // init Transform
-  stream.Transform.call(this, options);
+  return MagickStream;
 }
-util.inherits(Convert, stream.Transform);
 
-Convert.prototype._transform = function (chunk, enc, done) {
-  this._bufs.push(chunk);
-  done();
+module.exports.streams = { 
+  convert: getStreamHandler(module.exports.convert),
+  identify: getStreamHandler(module.exports.identify, true)
 };
-
-Convert.prototype._flush = function(done) {
-  this._options.srcData = Buffer.concat(this._bufs);
-  var self = this;
-  module.exports.convert(this._options,function(err,data){
-    if(err) {
-      return done(err);
-    }
-    self.push(data);
-    done();
-  });
-}
-
-module.exports.streams = { convert : Convert };
